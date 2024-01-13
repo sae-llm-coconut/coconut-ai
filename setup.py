4a3a3bc22ecbffd65e67e57fdbbb6764198104d5
Théo LUDWIG <contact@theoludwig.fr>
2024-01-14 00:27:33 +0100

    chore: cleanup

--- setup.py
+++ setup.py
@@ -5,14 +5,27 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="coconut-ai",
-    version="0.0.3",
+    version="0.1.0",
     author="Coconut AI",
     description="Coconut AI",
     url="https://github.com/sae-llm-coconut/coconut-ai",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires='>=3.10, <3.11',
-    package_dir={'':'src'},
+    package_dir={'': 'src'},
     packages=setuptools.find_packages(where="src"),
-    install_requires=[]
+    install_requires=[
+        'requests==2.31.0',
+        'pillow==10.2.0'
+    ],
+    extra_requires={
+        'dev': [
+            'autopep8==2.0.4',
+            'editorconfig-checker==2.7.3',
+            'mypy==1.8.0',
+            'setuptools',
+            'types-pillow==10.2.0.20240111',
+            'types-requests==2.31.0.20240106'
+        ]
+    }
 )
--- src/coconut_ai/coconut_ai.py
+++ src/coconut_ai/coconut_ai.py
@@ -1,20 +1,27 @@
-import json
 import requests
 import io
 import base64
-import urllib.request
-import os
 from typing import TypedDict
+
 from PIL import Image
 
 
+BASE_URL = "http://127.0.0.1:7861"
+
+
+def encode_file_to_base64(path: str) -> str:
+    with open(path, 'rb') as file:
+        return base64.b64encode(file.read()).decode('utf-8')
+
+
 class TextToImageOptions(TypedDict):
-    text: str
+    input_text: str
     output_path: str
+    steps: int
 
 
 class ImageToTextOptions(TypedDict):
-    image_path: str
+    input_path: str
 
 
 class TextToTextOptions(TypedDict):
@@ -22,56 +29,42 @@ class TextToTextOptions(TypedDict):
 
 
 class ImageToImageOptions(TypedDict):
-    image_path: str
+    input_path: str
     output_path: str
-
-url = "http://127.0.0.1:7860"
-
-def encode_file_to_base64(path: str)->str:
-    with open(path, 'rb') as file:
-        return base64.b64encode(file.read()).decode('utf-8')
-
-class CoconutAI:
-    def __init__(self) -> None:
-        pass
-
-    def text_to_image(self, options: TextToImageOptions) -> None:
-        print("---Text To Image---")
-        print(f"Text: {options['text']}")
-        payload = {
-            "prompt": options['text'],
-            "steps": 5
-        }
-
-        response = requests.post(url=f'http://127.0.0.1:7860/sdapi/v1/txt2img', json=payload)
-        r = response.json()
-        print(f"Output Image Path: {options['output_path']}")
-
-        image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
-        image.save(options['output_path'])
-
-    def image_to_text(self, options: ImageToTextOptions) -> str:
-        print("---Image To Text---")
-        print(f"Image Path: {options['image_path']}")
-        return "Text"
-
-    def text_to_text(self, options: TextToTextOptions) -> str:
-        print("---Text To Text---")
-        print(f"Text: {options['text']}")
-        return options["text"]
-
-    def image_to_image(self, options: ImageToImageOptions) -> None:
-        print("---Image To Image---")
-        print(f"Image Path: {options['image_path']}")
-        payload = {
-            "init_images": [encode_file_to_base64(options['image_path'])],
-            "steps": 5
-        }
-        response = requests.post(url=f'http://127.0.0.1:7860/sdapi/v1/img2img', json=payload)
-        r = response.json()
-        print(f"Output Image Path: {options['output_path']}")
-        image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
-        image.save(options['output_path'])
+    steps: int
 
 
+def text_to_image(options: TextToImageOptions) -> None:
+    payload = {
+        "prompt": options['input_text'],
+        "steps": options['steps']
+    }
+    response = requests.post(url=f'{BASE_URL}/sdapi/v1/txt2img', json=payload)
+    data = response.json()
+    image = Image.open(io.BytesIO(base64.b64decode(data['images'][0])))
+    image.save(options['output_path'])
+    image.close()
 
+
+def image_to_text(options: ImageToTextOptions) -> str:
+    print("---Image To Text---")
+    print(f"Image Path: {options['input_path']}")
+    return "Text"
+
+
+def text_to_text(options: TextToTextOptions) -> str:
+    print("---Text To Text---")
+    print(f"Text: {options['text']}")
+    return options["text"]
+
+
+def image_to_image(options: ImageToImageOptions) -> None:
+    payload = {
+        "init_images": [encode_file_to_base64(options['input_path'])],
+        "steps": options['steps']
+    }
+    response = requests.post(url=f'{BASE_URL}/sdapi/v1/img2img', json=payload)
+    data = response.json()
+    image = Image.open(io.BytesIO(base64.b64decode(data['images'][0])))
+    image.save(options['output_path'])
+    image.close()
